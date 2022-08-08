32a9d900f64bb61c720192864397b40692060a04
Michał Terczyński <mterczynski271@gmail.com>
2022-08-08 22:05:22 +0200

    Update template file names

---
+++ Git aliases.sh
@@ -0,0 +1,5 @@
+git config --global alias.ch checkout;
+git config --global alias.s status;
+git config --global alias.com commit;
+git config --global alias.f fetch;
+git config --global alias.m merge;
