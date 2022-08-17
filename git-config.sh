99de216cbe72542a5cdd98ada07496434253e37e
Michał Terczyński <mterczynski271@gmail.com>
2022-08-17 11:26:39 +0200

    Add "git config --global push.default current" to git settings

--- git-config.sh
+++ git-config.sh
@@ -3,3 +3,4 @@ git config --global alias.s status;
 git config --global alias.com commit;
 git config --global alias.f fetch;
 git config --global alias.m merge;
+git config --global push.default current;
