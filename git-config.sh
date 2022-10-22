03da17e707ef985a151b53086181d8db99749367
Michał Terczyński <mterczynski271@gmail.com>
2022-10-22 13:32:21 +0200

    Update git-config.sh

--- git-config.sh
+++ git-config.sh
@@ -1,6 +1,21 @@
+git config --global user.name "Michał Terczyński";
+git config --global user.email "mterczynski1@gmail.com";
 git config --global alias.ch checkout;
 git config --global alias.s status;
 git config --global alias.com commit;
 git config --global alias.f fetch;
 git config --global alias.m merge;
-git config --global push.default current;
+git config --global push.default current; # Don't require manual "--set-upstream" before first push on a new branch
+git config --global core.editor "vim";
+
+alias s="git status";
+alias a="git add .";
+alias f="git fetch";
+alias com="git commit";
+alias ch="git checkout";
+alias r="git reset";
+alias p="git pull";
+alias pull="git pull";
+alias merge="git merge";
+alias main="git checkout main || git checkout master";
+alias master="main";
