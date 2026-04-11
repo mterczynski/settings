9697e93e621b95f0e2e9367e18d90588479557ee
Michał Terczyński <mterczynski1@gmail.com>
2026-04-11 21:04:30 +0200

    Add Google Cloud SDK to WSL install script

--- setup/wsl/install-apps.sh
+++ setup/wsl/install-apps.sh
@@ -71,6 +71,11 @@ sudo apt update && sudo apt install terraform
 echo "Installing Docker Engine and Docker Compose..."
 curl -fsSL https://get.docker.com | sh
 
+# Google Cloud SDK
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update && sudo apt-get install -y google-cloud-cli
+
 echo ""
 echo "### All applications installed successfully ###"
 echo ""
